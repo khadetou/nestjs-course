7fff1166721431ab2539b3376a8fd4ccc3da4057
khadetou <khadetou96@gmail.com>
2021-12-17 19:26:28 +0000

    joi config

---
+++ src/config.schema.ts
@@ -0,0 +1,10 @@
+import * as Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+  STAGE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432).required(),
+  DB_USER: Joi.string().required(),
+  DB_PASS: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+});
