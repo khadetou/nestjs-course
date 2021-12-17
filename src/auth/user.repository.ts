d8b6d0360c97e4fcbd738000598f61a824343c62
khadetou <khadetou96@gmail.com>
2021-12-17 15:38:14 +0000

    user repository

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,29 @@
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { ConflictException } from '@nestjs/common';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    //Hash the password
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({
+      username,
+      password: hashPassword,
+    });
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(typeof error.code);
+      if (error.code === '23505') {
+        throw new ConflictException();
+      }
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('bootstrap');
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.useGlobalInterceptors(new TransformInterceptor());
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+  logger.log(process.env.STAGE);
 }
 bootstrap();
