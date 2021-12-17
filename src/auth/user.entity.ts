8acdbbbf992554ee741df4984dad3452b01c08a3
khadetou <khadetou96@gmail.com>
2021-12-17 11:34:59 +0000

    user entity

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,15 @@
+import { Task } from '../tasks/task.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+  @Column({ unique: true })
+  username: string;
+  @Column()
+  password: string;
+
+  //   @OneToMany((type)=> Task, (tasks) => tasks.user, {eager: true})
+  //   tasks: Task[];
+}
